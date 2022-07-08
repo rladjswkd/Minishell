59019e0fe8705209d4f642eedec5a34ef82bc141
Jake1152 <rancho974@gmail.com>
2022-07-09 02:43:55 +0900

    multi pipe test update

--- test/pipe_redirec_test.c
+++ test/pipe_redirec_test.c
@@ -83,19 +83,9 @@ static int	connect_pipe(int *fd, char **argv)
 	return (0);
 }
 
-static int	*make_pipe()
+static int	*make_pipe(int fd)
 {
-	int	*fd;
-
-	fd = (int *)malloc(sizeof(int) * 2);
-	if (fd == NULL)
-		return (NULL);
-	if (pipe(fd) < 0)
-	{
-		free(fd);
-		fd = NULL;
-	}
-	return (fd);
+	return (pipe(fd));
 }
 
 /*
@@ -114,18 +104,87 @@ static int	*make_pipe()
 	fork()를 뜬다.
 	
 */
+
+static int	connect_to_prev(t_fd *pipe_fd)
+{
+	if (dup2(pipe_fd[READ_END], STDIN_FILENO) < 0)
+		return (0);
+	close(pipe_fd[READ_END]);
+	return (1);
+}
+
+/*
+이미 닫힌 fd를 close할 경우 error가 발생할 수 있다.
+*/
+static int	connect_to_next(t_fd *pipe_fd)
+{
+	close(pipe_fd[READ_END]);
+	if (dup2(pipe_fd[WRITE_END], STDOUT_FILENO) < 0)
+		return (0);
+	close(pipe_fd[WRITE_END]);
+	return (0);
+}
+
+static int	is_exist_prev_pipe(size_t org_cnt, size_t cnt)
+{
+	if (org_cnt > cnt)
+		return (1);
+	return (0);
+}
+
+static int	is_exist_next_pipe(size_t cnt)
+{
+	if (cnt > 0)
+		return (1);
+	return (0);
+}
+
+static int	child_process(int pipe_org_cnt, int pipe_cnt, t_fd *pipe_fd)
+{
+	if (is_exist_prev_pipe(pipe_org_cnt, pipe_cnt))
+		connect_to_prev(pipe_fd);
+	if (is_exist_next_pipe(pipe_cnt))
+		connect_to_next(pipe_fd); //prev에서 쓰인것과 다른 fd여야한다.
+	return (0);
+}
+
+/*
+	현재가 오른쪽으로 연결하는지 왼쪽으로 연결하는지에 따라서 달라진다.
+	닫아야할 fd가 달라진다.
+	어떻게 구분해서 넘겨줄것인가
+	조건문을 덕지덕지 나눠서 넣지 않고 할수 있는 방법은 무엇인가?
+*/
+static int	parent_process(int *pipe_fd, pid_t pid, int status)
+{
+	// if
+	// close(pipe_fd[]);
+	waitpid(pid, &status, NULL)
+}
+
 int main(int argc, char **argv)
 {
-	int		*pipe_fd;
+	int		pipe_fd[2];
+	int		pid;
+	int		status;
 	// char	*cmd[] = {"ls", NULL};
-	size_t	multipipe_cnt;;
+	size_t	multipipe_cnt;
+	size_t	org_multipipe_cnt;
 
+	org_multipipe_cnt = 2;
 	multipipe_cnt = 2;
 	while (multipipe_cnt)
 	{
+		if (is_exist_next_pipe(multipipe_cnt))
+			if (pipe(pipe_fd) < 0)
+				return (1);
 		// printf("multipipe_cnt : %zu\n", multipipe_cnt);
-		pipe_fd = make_pipe();
-		fork();
+		pid = fork();
+		if (pid < 0)
+			return (1);
+		else if (pid == 0)
+			child_process(pipe_fd);
+		else
+			parent_process(pid, ,status);
 		if (pipe_fd == NULL)
 			return (1);
 		before_pipe(pipe_fd, argv);
