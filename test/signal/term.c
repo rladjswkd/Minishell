2cf80b71b1e2619e6ac59324d8c1dead1d9e51ea
Jake1152 <rancho974@gmail.com>
2022-07-14 20:58:50 +0900

    signal update

---
+++ test/signal/term.c
@@ -0,0 +1,55 @@
+#include <signal.h>
+#include <stdio.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <termios.h>
+
+void handler(int signum)
+{
+    if (signum != SIGINT)
+        return;
+	printf("^C\n");
+    rl_on_new_line();
+    rl_replace_line("", 1);
+    rl_redisplay();
+}
+
+void	set_signal()
+{
+	signal(SIGINT, handler);    // CTRL + C
+	// signal(SIGQUIT, SOG);
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	char			*str;
+	struct	termios	term;
+
+	tcgetattr(STDIN_FILENO, &term);
+	term.c_lflag &= ~(ECHOCTL);
+	tcsetattr(STDIN_FILENO, TCSANOW, &term);
+	set_signal();
+	while (1)
+	{
+		str = readline("pepsi zero__> ");
+		if (str == NULL)
+		{
+			printf("\033[1A");
+			printf("\033[10C");
+			printf(" exit\n");
+			exit(-1);
+		}
+		else if (*str == '\0')
+			free(str);
+		else
+		{
+			add_history(str);
+			printf("%s\n", str);
+			free(str);
+			str = NULL;
+		}
+	}
+	return (0);
+}
